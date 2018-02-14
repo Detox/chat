6a76458fdf91f55a5068e1838635c8b24b1ef1fc
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-02-14 02:45:31 +0200

    Continued prototyping

--- src/index.js
+++ src/index.js
@@ -9,16 +9,41 @@
     /**
      * @constructor
      *
-     * @param {!Object}	core_instance	Detox core instance
+     * @param {!Object}		core_instance	Detox core instance
+     * @param {Uint8Array=}	real_key_seed	Seed used to generate real long-term keypair (if not specified - random one is used)
      *
      * @return {!Chat}
      */
-    function Chat(core_instance){
+    function Chat(core_instance, real_key_seed){
+      real_key_seed == null && (real_key_seed = null);
       if (!(this instanceof Chat)) {
-        return new Chat(core_instance);
+        return new Chat(core_instance, real_key_seed);
       }
       asyncEventer.call(this);
+      this._core_instance = core_instance;
+      this._real_key_seed = real_key_seed || detoxCore['generate_seed']();
     }
+    Chat.prototype = {
+      /**
+       * Announce itself to the network (can operate without announcement)
+       *
+       * @param {number}	number_of_introduction_nodes
+       * @param {number}	number_of_intermediate_nodes	How many hops should be made until introduction node (not including it)
+       */
+      'announce': function(number_of_introduction_nodes, number_of_intermediate_nodes){
+        if (this._announced || this._destroyed) {
+          return;
+        }
+        this._announced = true;
+        this._core_instance['announce'](this._real_key_seed, number_of_introduction_nodes, number_of_intermediate_nodes);
+      },
+      'destroy': function(){
+        if (this._destroyed) {
+          return;
+        }
+        this._destroyed = true;
+      }
+    };
     Chat.prototype = Object.assign(Object.create(asyncEventer.prototype), Chat.prototype);
     Object.defineProperty(Chat.prototype, 'constructor', {
       enumerable: false,
--- src/index.ls
+++ src/index.ls
@@ -7,19 +7,38 @@ function Wrapper (detox-core, detox-crypto, fixed-size-multiplexer, async-evente
 	/**
 	 * @constructor
 	 *
-	 * @param {!Object}	core_instance	Detox core instance
+	 * @param {!Object}		core_instance	Detox core instance
+	 * @param {Uint8Array=}	real_key_seed	Seed used to generate real long-term keypair (if not specified - random one is used)
 	 *
 	 * @return {!Chat}
 	 */
-	!function Chat (core_instance)
+	!function Chat (core_instance, real_key_seed = null)
 		if !(@ instanceof Chat)
-			return new Chat(core_instance)
+			return new Chat(core_instance, real_key_seed)
 		async-eventer.call(@)
+
+		@_core_instance	= core_instance
+		@_real_key_seed	= real_key_seed || detox-core['generate_seed']()
 
 		# TODO
 
-	#Chat:: =
-		#TODO
+	Chat:: =
+		/**
+		 * Announce itself to the network (can operate without announcement)
+		 *
+		 * @param {number}	number_of_introduction_nodes
+		 * @param {number}	number_of_intermediate_nodes	How many hops should be made until introduction node (not including it)
+		 */
+		'announce' : (number_of_introduction_nodes, number_of_intermediate_nodes) !->
+			if @_announced || @_destroyed
+				return
+			@_announced	= true
+			@_core_instance['announce'](@_real_key_seed, number_of_introduction_nodes, number_of_intermediate_nodes)
+		# TODO: The rest of methods
+		'destroy' : !->
+			if @_destroyed
+				return
+			@_destroyed	= true
 
 	Chat:: = Object.assign(Object.create(async-eventer::), Chat::)
 
